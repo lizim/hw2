0b694ca1c1fd93b7509fefba04725c695cbced44
Live session user <mint@mint.(none)>
2015-11-08 19:18:48 +0000

    Update my hw2

--- logmap.cxx
+++ logmap.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -7,17 +8,26 @@ int main(){
 	double x;
 	int Nskip = 100; //Number of iterations to skip
 	int Nend  = 200; //Number of total iterations
-
+	double array[Nend-Nskip];
+	
+	ofstream out("hw2output.dat");
+	
 	for(double r=0; r <= 4; r += 0.001){
 	   x=x0;
 	   for(int i=1; i <= Nskip; i++)
 		   x = r*x*(1-x);
 	   for(int i=Nskip+1; i <= Nend; i++){
 	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
+	   		   array[i-Nskip-1] = x;
    	   }
+   	   
+   	   for(int i=0; i < Nskip; i++){
+		   out << "r = " << r << " \t x = " << x << endl;
+		   
+	   }
+	  
 	}
-
+	out.close();
 
 	return 0;
 }
