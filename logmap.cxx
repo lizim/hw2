7e8208c0d4c87dca5e0eb244b8f75b5fb1214956
Live session user <mint@mint.(none)>
2015-11-08 19:41:27 +0000

    Update hw2 lizim

--- logmap.cxx
+++ logmap.cxx
@@ -22,7 +22,7 @@ int main(){
    	   }
    	   
    	   for(int i=0; i < Nskip; i++){
-		   out << "r = " << r << " \t x = " << x << endl;
+		   out << "r = " << r << " \t x = " << array[i] << endl;
 		   
 	   }
 	  
